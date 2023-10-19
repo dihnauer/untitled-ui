0827bfa2bcc293397c92d0e29b73d717d83e6066
Diogo Hanauer <dihnauer@gmail.com>
2023-10-19 17:26:56 -0300

    create context to share id between control and trigger components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
-              <FileInput.Control />
+              <FileInput.Control multiple />
             </FileInput.Root>
           </div>
 
--- src/components/FileInput/Root.tsx
+++ src/components/FileInput/Root.tsx
@@ -1,7 +1,23 @@
-import { ComponentProps } from "react";
+"use client";
+
+import { ComponentProps, createContext, useContext, useId } from "react";
+
+type FileInputContextType = {
+  id: string;
+};
+
+const FileInputContext = createContext({} as FileInputContextType);
+
+export const useFileInput = () => useContext(FileInputContext);
 
 interface RootProps extends ComponentProps<"div"> {}
 
 export function Root(props: RootProps) {
-  return <div {...props} />;
+  const id = useId();
+
+  return (
+    <FileInputContext.Provider value={{ id }}>
+      <div {...props} />;
+    </FileInputContext.Provider>
+  );
 }
