2cb89decb07a080b60e65ae80fd2ebfadf58664f
Diogo Hanauer <dihnauer@gmail.com>
2023-10-19 17:35:53 -0300

    create imagepreview by selecting an image

--- src/components/FileInput/Root.tsx
+++ src/components/FileInput/Root.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { ComponentProps, createContext, useContext, useId } from "react";
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from "react";
 
 type FileInputContextType = {
   id: string;
+  files: File[];
+  onFilesSelected: (files: File[]) => void;
 };
 
 const FileInputContext = createContext({} as FileInputContextType);
@@ -14,10 +22,11 @@ interface RootProps extends ComponentProps<"div"> {}
 
 export function Root(props: RootProps) {
   const id = useId();
+  const [files, setFiles] = useState<File[]>([]);
 
   return (
-    <FileInputContext.Provider value={{ id }}>
-      <div {...props} />;
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+      <div {...props} />
     </FileInputContext.Provider>
   );
 }
