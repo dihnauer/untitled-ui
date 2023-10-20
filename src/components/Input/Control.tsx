22f2c145a33cb9df54a0f3c39f6b3c44a809af81
Diogo Hanauer <dihnauer@gmail.com>
2023-10-20 14:57:27 -0300

    add dark theme to sidebar and its components

--- src/components/Input/Control.tsx
+++ src/components/Input/Control.tsx
@@ -5,7 +5,7 @@ interface ControlProps extends ComponentProps<"input"> {}
 export function Control(props: ControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 outline-none placeholder:text-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 outline-none placeholder:text-zinc-600 dark:text-zinc-100 dark:placeholder-zinc-400"
       {...props}
     />
   );
