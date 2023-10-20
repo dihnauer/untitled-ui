2f7cb2a71754498055543e8af197ed5e92ecaa97
Diogo Hanauer <dihnauer@gmail.com>
2023-10-20 13:40:37 -0300

    add slot variants to button component

--- src/components/FileInput/FileItem.tsx
+++ src/components/FileInput/FileItem.tsx
@@ -1,33 +1,63 @@
 import { CheckCircle2, Trash2, UploadCloud } from "lucide-react";
 import { Button } from "../Button";
 import { formatBytes } from "@/utils/format-bytes";
+import { VariantProps, tv } from "tailwind-variants";
 
-interface FileItemProps {
+const fileItem = tv({
+  slots: {
+    container:
+      "group flex items-start gap-4 rounded-lg border border-zinc-200 p-4",
+    icon: "rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600",
+    deleteButton: "",
+  },
+
+  variants: {
+    state: {
+      progress: {
+        container: "",
+      },
+      complete: {
+        container: "border-violet-500",
+      },
+      error: {
+        container: "bg-error-25 border-error-300",
+        icon: "border-error-50 bg-error-100 text-error-600",
+        deleteButton: "text-error-700 hover:text-error-900",
+      },
+    },
+  },
+
+  defaultVariants: {
+    state: "progress",
+  },
+});
+
+interface FileItemProps extends VariantProps<typeof fileItem> {
   name: string;
   size: number;
 }
 
-export function FileItem({ name, size }: FileItemProps) {
-  const state = "ERROR" as "PROGRESS" | "ERROR" | "COMPLETE";
+export function FileItem({ name, size, state }: FileItemProps) {
+  const { container, icon, deleteButton } = fileItem({ state });
 
   return (
-    <div className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4">
-      <div className="rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600">
+    <div className={container()}>
+      <div className={icon()}>
         <UploadCloud className="h-4 w-4" />
       </div>
 
-      {state === "ERROR" ? (
+      {state === "error" ? (
         <div className="flex flex-1 flex-col items-start gap-1">
           <div className="flex flex-col">
-            <span className="text-error-700 text-sm font-medium">
+            <span className="text-sm font-medium text-error-700">
               Upload failed, please try again.
             </span>
-            <span className="text-error-600 text-sm">{name}</span>
+            <span className="text-sm text-error-600">{name}</span>
           </div>
 
           <button
             type="button"
-            className="text-error-700 hover:text-error-900 text-sm font-semibold"
+            className="text-sm font-semibold text-error-700 hover:text-error-900"
           >
             Try again
           </button>
@@ -43,22 +73,22 @@ export function FileItem({ name, size }: FileItemProps) {
             <div className="h-2 flex-1 rounded-full bg-zinc-100">
               <div
                 className="h-2 rounded-full bg-violet-600"
-                style={{ width: state === "COMPLETE" ? "100%" : "80%" }}
+                style={{ width: state === "complete" ? "100%" : "80%" }}
               />
             </div>
 
             <span className="text-sm font-medium text-zinc-700">
-              {state === "COMPLETE" ? "100%" : "80%"}
+              {state === "complete" ? "100%" : "80%"}
             </span>
           </div>
         </div>
       )}
 
-      {state === "COMPLETE" ? (
+      {state === "complete" ? (
         <CheckCircle2 className="h-5 w-5 fill-violet-600 text-white" />
       ) : (
-        <Button type="button" variant="ghost">
-          <Trash2 className="h-5 w-5 text-zinc-500" />
+        <Button type="button" variant="ghost" className={deleteButton()}>
+          <Trash2 className="h-5 w-5" />
         </Button>
       )}
     </div>
