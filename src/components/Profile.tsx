26ed6835e634e887a661298f04242cf4684e6fe0
Diogo Hanauer <dihnauer@gmail.com>
2023-10-20 11:07:20 -0300

    add button variant ghost

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from "lucide-react";
+import { Button } from "./Button";
 
 export function Profile() {
   return (
@@ -9,7 +10,7 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
 
-      <div className="flex flex-col truncate">
+      <div className="mr-auto flex flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700">
           Diogo Hanauer
         </span>
@@ -18,9 +19,9 @@ export function Profile() {
         </span>
       </div>
 
-      <button type="button" className="ml-auto rounded-lg p-2 hover:bg-zinc-50">
+      <Button type="button" variant="ghost">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   );
 }
